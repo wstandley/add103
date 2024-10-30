412afd699c1ae37d8407e0d02ebc6627f1c98d92
wstandley <williamstandley03@gmail.com>
2024-10-30 14:57:47 -0500

    Assignment: Grid

---
+++ grid/change.js
@@ -0,0 +1,20 @@
+function change01() {
+  document.getElementById("large").src = "images/duck-in-boots-rotate.jpeg";
+}
+
+function change02() {
+  document.getElementById("large").src = "images/duck-pinball-rotate.jpeg";
+}
+
+function change03() {
+  document.getElementById("large").src = "images/ducks-in-vegas-flip.jpeg";
+}
+
+function change04() {
+  document.getElementById("large").src =
+    "images/grand-canyon-ducks-resized.jpeg";
+}
+
+function change05() {
+  document.getElementById("large").src = "images/light-ducks-crop.jpeg";
+}
